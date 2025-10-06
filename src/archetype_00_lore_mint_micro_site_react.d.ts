7a4f0214e764f194e9c8f515a0c7fd67f7235b0a
Pecherius <modorepano@gmail.com>
2025-10-06 03:26:23 -0300

    Fix TypeScript declaration path

--- src/archetype_00_lore_mint_micro_site_react.d.ts
+++ src/archetype_00_lore_mint_micro_site_react.d.ts
@@ -1,4 +1,4 @@
-declare module '../archetype_00_lore_mint_micro_site_react' {
+declare module './ArchetypeSite' {
   import React from 'react';
   const ArchetypeSite: React.FC;
   export default ArchetypeSite;
